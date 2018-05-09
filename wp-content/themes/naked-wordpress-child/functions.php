1716282edf60047c4f73f6727b559c09fc076876
Mariano <mariano@upfordev.com>
2018-05-09 11:42:06 -0300

    Fix: do not enqueue less style for admin

--- wp-content/themes/naked-wordpress-child/functions.php
+++ wp-content/themes/naked-wordpress-child/functions.php
@@ -1,6 +1,5 @@
 <?php
 
-//function naked_child_scripts() {
+if ( ! is_admin() ) {
   wp_enqueue_style('style.less', get_stylesheet_directory_uri() . '/style.less');
-//}
-//add_action( 'naked_child_scripts' );
+}
